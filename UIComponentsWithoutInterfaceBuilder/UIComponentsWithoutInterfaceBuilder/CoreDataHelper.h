be4fa255f646cb9303edf48e7b10133f99dc85fc
LolaChernysheva <lolita-chernysheva@mail.ru>
2021-01-29 13:26:36 +0300

    Добавлен класс CoreDataHelper
    Добавлен класс CoreDataHelper

--- UIComponentsWithoutInterfaceBuilder/UIComponentsWithoutInterfaceBuilder/CoreDataHelper.h
+++ UIComponentsWithoutInterfaceBuilder/UIComponentsWithoutInterfaceBuilder/CoreDataHelper.h
@@ -7,10 +7,21 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+#import "DataManager.h"
+#import "FavoriteTicket+CoreDataClass.swift"
+#import "Ticket.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface CoreDataHelper : NSObject
+
++ (instancetype)sharedInstance;
+
+- (BOOL)isFavorite:(Ticket *)ticket;
+- (NSArray *)favorites;
+- (void)addToFavorite:(Ticket *)ticket;
+- (void)removeFromFavorite:(Ticket *)ticket;
 
 @end
 
