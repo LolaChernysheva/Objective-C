24a865840af5cffd2778ae0294357ab2600a6a33
LolaChernysheva <lolita-chernysheva@mail.ru>
2021-01-29 12:43:37 +0300

    Использование NSUserDefaults
    Использование NSUserDefaults

---
+++ NSUserDFL/NSUserDFL/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  NSUserDFL
+//
+//  Created by Лолита on 29.01.2021.
+//  Copyright © 2021 Lolita Chernysheva. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ NSUserDFL/NSUserDFL/ViewController.m
@@ -0,0 +1,39 @@
+//
+//  ViewController.m
+//  NSUserDFL
+//
+//  Created by Лолита on 29.01.2021.
+//  Copyright © 2021 Lolita Chernysheva. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    NSString *name = [defaults stringForKey:@"name"]; // Чтение настроек по ключу name
+    NSInteger age = [defaults integerForKey:@"age"]; // Чтение настроек по ключу age
+
+    NSLog(@"name - %@, age - %ld", name, (long)age);
+
+    if (!name || !age) {
+        [defaults setObject:@"Steve" forKey:@"name"]; // Сохраняется имя Steve
+        [defaults setInteger:56 forKey:@"age"]; // Сохраняется значение 56 для ключа age
+    }
+
+    NSLog(@"name - %@, age - %ld", name, (long)age);
+
+    name = [defaults stringForKey:@"name"];
+    age = [defaults integerForKey:@"age"];
+
+    NSLog(@"name - %@, age - %ld", name, (long)age);
+}
+
+
+@end
