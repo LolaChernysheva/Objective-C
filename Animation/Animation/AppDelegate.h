80766d7aa6f40a099a741a13b065932c438eb997
LolaChernysheva <lolita-chernysheva@mail.ru>
2021-02-02 14:10:46 +0300

    Animation
    Animation

---
+++ Animation/Animation/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Animation
+//
+//  Created by Лолита on 02.02.2021.
+//  Copyright © 2021 Lolita Chernysheva. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Animation/Animation/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Animation
+//
+//  Created by Лолита on 02.02.2021.
+//  Copyright © 2021 Lolita Chernysheva. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
